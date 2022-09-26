ddb15eac91e14bc9e7ff6d18bfa743c8d825c6b0
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-26 10:42:11 +0100

    removed useless import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProfilePage from "./components/ProfilePage";
 
 function App() {
