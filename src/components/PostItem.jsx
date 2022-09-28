7bdd6b60b426faa02f99bb640de2e86f0a10add2
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-28 10:19:05 +0100

    added notes for nazami

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -7,7 +7,12 @@ const PostItem = ({post}) => {
       }}
       className="text-left position-relative my-2"
     >
-      <h2>{post.text}</h2>
+    {/* TEMPALTE START */}
+        {/* NAZAMI PUT YOUR TEMPLATE HERE  */}
+        {/* the post is passed as props to this component as an object, check discord for how the response is returned */}
+        {/* EXAMPLES: post.text, post.user.name, post.user.title */}
+      
+      {/* TEMPLATE END */}
     </div>
   );
 };
