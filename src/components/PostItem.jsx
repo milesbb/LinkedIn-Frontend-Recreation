2940503d348550c3f72642be126fece7f81e1eee
mohammadnazamikit <108289537+mohammadnazamikit@users.noreply.github.com>
2022-09-28 15:47:30 +0200

    post updating

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,3 +1,7 @@
+import { Card } from "react-bootstrap";
+import {Button} from "react-bootstrap";
+
+
 const PostItem = ({post}) => {
   return (
     <div
@@ -7,11 +11,65 @@ const PostItem = ({post}) => {
       }}
       className="text-left position-relative my-2"
     >
+
+<Card>
+<Card.Title className="p-3">
+{/* <div className='container' style={{marginTop:'10px',marginLeft:'5px',padding:'0px'}} >
+                      <div className='row' >
+                          <div className='col-2'>
+                          <img width="30" className="" style={{borderRadius:15}} src={post.user.image}/>
+                          </div>
+                          <div className='col-10'  >
+                          <h5 style={{fontWeight:'bold'}}>{post.user.name+' '+post.user.surname}</h5>
+                          <h6 >{post.user.title}</h6>
+                          <h6 >{post.user.company}</h6>
+                          </div>
+                      </div>
+                  </div> */}
+
+
+<div className="d-flex">
+<img className="mr-2 mt-1"  style={{borderRadius:'25px',width:'50px',height:'50px'}} src={post.user.image}/>
+  <div>
+    <h5 className="font-weight-bold">{post.user.name+' '+post.user.surname}</h5>
+    <h6 >{post.user.title}</h6>
+     <h6 >{post.user.company}</h6>
+
+  </div>
+</div>
+
+
+  </Card.Title>
+  <Card.Text>
+    <div className="w-75 ml-4">
+    {post.text}
+    </div>
+        </Card.Text>
+    {/*   <Card.Img variant="top" src={post.user.image} /> */}
+      <Card.Body>
+        
+        <Button variant="light" className="w-25" style={{fontSize:'0.8rem'}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+  <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+</svg> Like</Button>
+        <Button variant="light" className="w-25" style={{fontSize:'0.8rem'}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-right-text" viewBox="0 0 16 16">
+  <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z"/>
+  <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+</svg> Comment</Button>
+        <Button variant="light" className="w-25" style={{fontSize:'0.8rem'}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-90deg-right" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M14.854 4.854a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 4H3.5A2.5 2.5 0 0 0 1 6.5v8a.5.5 0 0 0 1 0v-8A1.5 1.5 0 0 1 3.5 5h9.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4z"/>
+</svg> Share</Button>
+        <Button variant="light" className="w-25" style={{fontSize:'0.8rem'}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
+  <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
+</svg> Send</Button>
+      </Card.Body>
+    </Card>
+
+
     {/* TEMPALTE START */}
         {/* NAZAMI PUT YOUR TEMPLATE HERE  */}
         {/* the post is passed as props to this component as an object, check discord for how the response is returned */}
         {/* EXAMPLES: post.text, post.user.name, post.user.title */}
-      <p>Test display text: {post.text}</p>
+     
       {/* TEMPLATE END */}
     </div>
   );
