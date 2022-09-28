0a74cfdd03b3fd667e837f210fa34863a39813e2
mohammadnazamikit <108289537+mohammadnazamikit@users.noreply.github.com>
2022-09-28 09:42:15 +0200

    spacing between tabs in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const NavFunction=()=> {
   return (
     <Navbar  bg="light" expand="lg" style={{padding:0,margin:0,height:'50px'}}>
       <Container >
-        <Navbar.Brand href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="blue" class="mercado-match" width="40" height="40" focusable="false">
+        <Navbar.Brand href="#" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="#0a66c2" class="mercado-match" width="40" height="40" focusable="false">
   <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
 </svg>
 </Navbar.Brand>
@@ -24,12 +24,12 @@ const NavFunction=()=> {
                     <Form.Group >
                         <InputGroup >
                             <InputGroup.Prepend>
-                                <InputGroup.Text style={{backgroundColor:"#eef3f8", height:'40px'}}>
+                                <InputGroup.Text style={{backgroundColor:'white', height:'40px',border:'0px'}}>
                                 <FaSearch/>
                                   </InputGroup.Text>
                             </InputGroup.Prepend>
                             <Form.Control
-                                type="text" style={{height:'40px'}}
+                                type="text" style={{height:'40px', border:'0px'}}
                                 placeholder="Search"
                             />
                         </InputGroup>
@@ -50,7 +50,7 @@ const NavFunction=()=> {
 <h6  style={{fontSize:'12px'}}>home</h6>
                 </div>
                 </Nav.Link>
-            <Nav.Link href="#action2">
+            <Nav.Link href="#action2" style={{marginLeft:'15px'}}>
                 <div className='position-relative mt-3'>
                     <div>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
@@ -63,7 +63,7 @@ const NavFunction=()=> {
 </h6>
                 </div>
             </Nav.Link>
-           <Nav.Link>
+           <Nav.Link  style={{marginLeft:'15px'}}>
             <div className='position-relative mt-3' >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
   <path d="M22.84 10.22L21 6h-3.95V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2l2.22 5.18A3 3 0 007 13h14a2 2 0 001.84-2.78zM15.05 6h-6V5a1 1 0 011-1h4a1 1 0 011 1zM7 14h15v3a3 3 0 01-3 3H5a3 3 0 01-3-3V8.54l1.3 3A4 4 0 007 14z"></path>
@@ -73,7 +73,7 @@ const NavFunction=()=> {
 </h6>
             </div>
            </Nav.Link>
-           <Nav.Link>
+           <Nav.Link style={{marginLeft:'15px'}}>
             <div className='position-relative mt-3'>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
   <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
@@ -83,7 +83,7 @@ const NavFunction=()=> {
 </h6>
             </div>
            </Nav.Link>
-           <Nav.Link>
+           <Nav.Link style={{marginLeft:'15px'}}>
             <div className='position-relative mt-3'>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
   <path d="M22 19.24a3.46 3.46 0 01-.09.78l-.22 1-6.76-1.51A2.16 2.16 0 0115 20a2 2 0 11-3.53-1.28L2 16.62l.22-1A4.45 4.45 0 014 13.12l1.22-.93 15.46 3.44.7 1.36a5 5 0 01.62 2.25zm-1.49-10.4a6.29 6.29 0 00-4.92-6.69A6.76 6.76 0 0014.18 2a6.29 6.29 0 00-5.9 4.12l-2 5.27 13.8 3.08z"></path>
@@ -94,7 +94,7 @@ const NavFunction=()=> {
             </div>
            </Nav.Link>
           
-            <NavDropdown className='btn position-relative mt-2' title={
+            <NavDropdown style={{marginLeft:'15px'}} className='btn position-relative mt-2' title={
                 <>
                 <div>
                 <img width="24" style={{borderRadius:12}} src="https://media-exp1.licdn.com/dms/image/C4D03AQEcB_fj3rDIDg/profile-displayphoto-shrink_100_100/0/1645373970103?e=1669852800&amp;v=beta&amp;t=ouL_3xtMaDnvqOSJlD9D0QmvYzQX4lpfITvqp4cOe30" height="24" alt="mohammad jan nazami" id="ember19" class="global-nav__me-photo ember-view"/>
@@ -229,11 +229,10 @@ const NavFunction=()=> {
                     </a>
                 </div>
                 </>
-
                 }</NavDropdown.Item>
             
             </NavDropdown>
-            <Nav.Link href="#" style={{color:'#915907', textDecoration:'underline'}} className='postion-relative mt-4'>
+            <Nav.Link href="#" style={{color:'#915907', textDecoration:'underline',fontSize:'12px'}} className='postion-relative mt-4'>
               try Primium for <br/> free
             </Nav.Link>
           </Nav>
